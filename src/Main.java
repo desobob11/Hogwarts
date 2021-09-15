5efaab8374ed311b95d5b731c4c71e2352af22e7
Desmondob11 <desmond.obrien@ucalgary.ca>
2021-09-15 10:27:45 -0600

    BIG CHANGES AGAIN BABY

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,9 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		Application app = new Application();
-		app.beginGame();
+		//Application app = new Application();
+		//app.beginGame();
+		System.out.println("SUCCESS AGAIN!");
 
 		}
 
